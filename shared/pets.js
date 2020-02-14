3b18ef804e8405271c01630350b997ea52e9d315
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-02-14 11:49:37 -0600

    add beginning content for pets database

--- shared/pets.js
+++ shared/pets.js
@@ -1,1 +1,83 @@
 //adoptable animal database
+export const PETS = [
+  {
+    id: 0,
+        name: "Andromeda",
+        image: '../components/images/andromeda.jpg',
+        featured: false,
+        info: "Male Kitten Domestic Shorthair",
+        description: "Meet Andromeda! Andromeda is around 12 weeks old and came to us from a rural Alabama shelter with his siblings."
+  },
+  {
+    id: 1,
+        name: "Bella",
+        image: '../components/images/bella.jpg',
+        featured: false,
+        info: "Female Young Mixed Breed",
+        description: "Meet Bella! Bella is about 4-months-old and 30-pounds. She ended up at a rural Alabama shelter and with no adoption prospects. She is a sweet girl who loves other dogs and people!"
+  },
+  {
+    id: 2,
+        name: ,
+        image: ,
+        featured: false,
+        info: ,
+        description: 
+  },
+  {
+    id: 3,
+        name: ,
+        image: ,
+        featured: false,
+        info: ,
+        description: 
+  },
+  {
+    id: 4,
+        name: ,
+        image: ,
+        featured: false,
+        info: ,
+        description: 
+  },
+  {
+    id: 5,
+        name: ,
+        image: ,
+        featured: false,
+        info: ,
+        description: 
+  },
+  {
+    id: 6,
+        name: ,
+        image: ,
+        featured: false,
+        info: ,
+        description: 
+  },
+  {
+    id: 7,
+        name: ,
+        image: ,
+        featured: false,
+        info: ,
+        description: 
+  },
+  {
+    id: 8,
+        name: ,
+        image: ,
+        featured: false,
+        info: ,
+        description: 
+  },
+  {
+    id: 9,
+        name: ,
+        image: ,
+        featured: false,
+        info: ,
+        description: 
+  }
+]
