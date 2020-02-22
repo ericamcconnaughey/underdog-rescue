563010b267994cb41dd2b3a42a813a5898c202e9
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-02-21 22:43:32 -0600

    install RNE, R Navigation, and Animatable

--- components/AdoptComponent.js
+++ components/AdoptComponent.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Text, View, ScrollView, FlatList, 
+  Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
+const mapStateToProps = state
+function RenderPet(props) {
+
+}
 
 
 export default Adopt;
