e4e39a6adc3fdcc864743b698d14fc3c73cbf0c9
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-02-21 21:54:07 -0600

    add adoptable pet data

--- shared/pets.js
+++ shared/pets.js
@@ -3,81 +3,91 @@ export const PETS = [
   {
     id: 0,
         name: "Andromeda",
-        image: '../components/images/andromeda.jpg',
+        image: "../components/images/andromeda.jpg",
         featured: false,
+        type: "cat",
         info: "Male Kitten Domestic Shorthair",
         description: "Meet Andromeda! Andromeda is around 12 weeks old and came to us from a rural Alabama shelter with his siblings."
   },
   {
     id: 1,
         name: "Bella",
-        image: '../components/images/bella.jpg',
+        image: "../components/images/bella.jpg",
         featured: false,
+        type: "dog",
         info: "Female Young Mixed Breed",
         description: "Meet Bella! Bella is about 4-months-old and 30-pounds. She ended up at a rural Alabama shelter and with no adoption prospects. She is a sweet girl who loves other dogs and people!"
   },
   {
     id: 2,
-        name: ,
-        image: ,
+        name: "Butters",
+        image: "../components/images/butters.jpeg",
         featured: false,
-        info: ,
-        description: 
+        type: "dog",
+        info: "Male Young Mixed Breed",
+        description: "Meet Butters! Butters is around 8 months old and 40lbs. He ended up in a rural Alabama shelter that euthanizes for space weekly. He is friendly with people and loves other dogs. Now in WI, he is working on basic training skills. He is an active young dog who would love an active family to share adventures with him. He is such a sweet and handsome boy who deserves an amazing forever family."
   },
   {
     id: 3,
-        name: ,
-        image: ,
+        name: "Luna",
+        image: "../components/images/luna.jpeg",
         featured: false,
-        info: ,
-        description: 
+        type: "cat",
+        info: "Female Kitten Domestic Shorthair Tabby (mixed)",
+        description: "Meet Luna! About 6 weeks old (2/21) Luna came into rescue with her siblings and mom, Violet, but sadly she was the only survivor. This sweet girl would love to have a warm, soft space to land in Wisconsin!"
   },
   {
     id: 4,
-        name: ,
-        image: ,
+        name: "Pepper",
+        image: "../components/images/luna.jpg",
         featured: false,
-        info: ,
-        description: 
+        type: "dog",
+        info: "Male Young Mixed Breed",
+        description: "Meet Pepper! Pepper is about 8-months-old and 15-pounds. He was dumped on a country road in Texas. He is really friendly, working on house-training, and good with kids and other dogs!"
   },
   {
     id: 5,
-        name: ,
-        image: ,
+        name: "Scooter",
+        image: "../components/images/scooter.jpg",
         featured: false,
-        info: ,
-        description: 
+        type: "dog",
+        info: "Male Adult Mixed Breed",
+        description: "Meet Scooter! Scooter is about 4-years-old and 30-pounds. He was acquired by a woman to hunt squirrels in her yard. But, he wasn't a fan of this job, so after a week she left him in the overnight drop box at a rural Alabama shelter. He's a sweetheart and a love-bug, so the shelter really wanted Underdog to take him! What Scooter wants most in life is getting love and attention. Scooter is a little timid and would love a patient family to give him time to settle in."
   },
   {
     id: 6,
-        name: ,
-        image: ,
+        name: "Truffle",
+        image: "../components/images/truffle.jpeg",
         featured: false,
-        info: ,
-        description: 
+        type: "cat",
+        info: "Young Male Domestic Shorthair",
+        description: "Meet Truffle! He's part of our Sweet Treats litter with Pepper & Mocha, 6 months-old (1/28), sweet, and stunning! Truffle is a handsome lovebug who cuddles with his whole body. Likes: Watching TV, playing computer games, hiking around his home, naps, bags, and boxes "
   },
   {
     id: 7,
-        name: ,
-        image: ,
+        name: "Tundra",
+        image: "../components/images/tundra.jpg",
         featured: false,
-        info: ,
-        description: 
+        type: "dog",
+        info: "Female Young Mixed Breed",
+        description: "Meet Tundra! Tundra is around 5 months old and 25lbs. Tundra had an intestinal issue and has now had surgery to fix it, but may need a special prescription diet for life as a result. This gorgeous girl loves cuddling, playing with toys and going for walks. She is crate trained and she's doing great with house training for a 5 month old pup and now goes to the door to signal at times."
   },
   {
     id: 8,
-        name: ,
-        image: ,
+        name: "Violet",
+        image: "../components/images/violet.jpeg",
         featured: false,
-        info: ,
-        description: 
+        type: "cat",
+        info: "Female Adult Tortoiseshell",
+        description: "Meet Violet! This mama raised her singlet kitten, Luna, and is ready to find a home of her own!"
   },
   {
     id: 9,
-        name: ,
-        image: ,
+        name: "Winnie",
+        image: "../components/images/winnie.jpeg",
         featured: false,
-        info: ,
-        description: 
+        type: "dog",
+        info: "Female Adult Mixed Breed",
+        description: "Meet Winnie! Winnie is about 1-year-old and 41-pounds. She ended up in a rural Alabama shelter, with a limp. The Underdog veterinarians decided that FHO surgery was her best option. She had this done on 2/20 is recovering very well. Winnie is the sweetest, floppiest, smiling dog you will ever meet. Everyone is her best friend and she is just so excited about life. She gets along with dogs of all sizes and ages, cats, and kids."
   }
 ]
