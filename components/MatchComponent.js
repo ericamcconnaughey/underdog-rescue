c654a90bc6166b381fb211c456e68a45b900abcc
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-02-26 14:33:25 -0600

    add Swipe component with functional pet cards

--- components/MatchComponent.js
+++ components/MatchComponent.js
@@ -1,60 +1,148 @@
 import React, { Component } from 'react';
 import { PETS } from '../shared/pets';
-import { Text, View, ScrollView, FlatList, 
-  Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
-import { Card, Icon, ListItem } from 'react-native-elements';
-//import * as Animatable from 'react-native-animatable';
+import Swipe from './SwipeComponent';
+import { Text, View, Image, StyleSheet, SafeAreaView } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+// import SafeAreaView from 'react-native-safe-area-view';
 
 class Match extends Component {
+  state = {
+    likedPets: 0,
+    passedPets: 0
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      pets: PETS
-    };
+  handleLikedPet = () => {
+    this.setState(({ likedPets }) => ({
+      likedPets: likedPets + 1
+    }));
+  };
+
+  handlePassedPet = () => {
+    this.setState(({ passedPets }) => ({
+      passedPets: passedPets + 1
+    }));
+  };
+
+  renderCards(pet) {
+    return (
+      <Card
+        title={pet.name}>
+        <View style={{ height: 200 }}>
+          <Image source={require('./images/bella.jpg')}
+            style={{ width: '100%', height: 200 }} />
+        </View>
+        <View style={styles.details}>
+          <Text style={{margin: 10}}>{pet.info} {pet.type}</Text>
+          <Text style={{margin: 10}}>{pet.description}</Text>
+        </View>
+      </Card>
+    );
   }
 
-  static navigationOptions = {
-    title: 'Pet Match'
-  };
-
-  render() {
-    const { navigate } = this.props.navigation;
-    // const petId = this.props.navigation.getParam('petId');
-    // const pet = this.state.pets.filter(pet => pet.id === petId)[0];
-    const renderAdoptItem = ({item}) => {
-      return (
-          <ListItem
-              title={item.name}
-              subtitle={item.type}
-              onPress={() => navigate('PetInfo', {petId: item.id})}
-              leftAvatar={{ source: require('./images/bella.jpg')}}
-          />
-      );
-    };
-
-
+  renderNoMoreCards = () => {
     return (
-      <View>
-        <Text style={styles.headerText}>Our Adoptables</Text>
-        <FlatList
-          data={this.state.pets}
-          renderItem={renderAdoptItem}
-          keyExtractor={item => item.id.toString()}
-          />
-        </View>
+      <Card title="There are No More Adoptable Pets">
+        <Button
+          title="Do Something"
+          large
+          icon={{ name: 'my-location' }}
+          backgroundColor='#03A9F4' />
+      </Card>
     );
   };
+  
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.status}>
+          <Text>Passed: {this.state.passedPets}</Text>
+          <Text>Liked: {this.state.likedPets}</Text>
+        </View>
+        <Swipe 
+          onSwipeRight={this.handleLikedPet}
+          onSwipeLeft={this.handlePassedPet}
+          data={PETS}
+          keyProp="petId"
+          renderCard={this.renderCards}
+          renderNoMoreCards={this.renderNoMoreCards} />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  headerText: {
-    justifyContent: 'center',
-    alignContent: 'center',
-    margin: 20,
-    fontSize: 20,
-    fontWeight: 'bold'
+  container: {
+    flex: 1,
+    backgroundColor: '#F6EFE4',
   },
-})
+  status: {
+    paddingTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  // details: {
+
+  // }
+});
 
 export default Match;
+
+
+
+
+
+
+
+
+// OLD MATCH COMPONENT
+// class Match extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       pets: PETS
+//     };
+//   }
+
+//   static navigationOptions = {
+//     title: 'Pet Match'
+//   };
+
+//   render() {
+//     const { navigate } = this.props.navigation;
+//     const renderAdoptItem = ({item}) => {
+//       return (
+//           <ListItem
+//               title={item.name}
+//               subtitle={item.type}
+//               onPress={() => navigate('PetInfo', {petId: item.id})}
+//               leftAvatar={{ source: require('./images/bella.jpg')}}
+//           />
+//       );
+//     };
+
+
+//     return (
+//       <View>
+//         <Text style={styles.headerText}>Our Adoptables</Text>
+//         <FlatList
+//           data={this.state.pets}
+//           renderItem={renderAdoptItem}
+//           keyExtractor={item => item.id.toString()}
+//           />
+//         </View>
+//     );
+//   };
+// }
+
+// const styles = StyleSheet.create({
+//   headerText: {
+//     justifyContent: 'center',
+//     alignContent: 'center',
+//     margin: 20,
+//     fontSize: 20,
+//     fontWeight: 'bold'
+//   },
+// })
+
---
+++ components/SwipeComponent.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+import { View, Text, PanResponder, Animated, Dimensions, StyleSheet, UIManager, LayoutAnimation, Platform, } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
+
+class Swipe extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {},
+    keyProp: 'id'
+  };
+
+  constructor(props) {
+    super(props);
+    this.position = new Animated.ValueXY();
+
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (e, gestureState) => true,
+      onPanResponderMove: (e, gestureState) => {
+        this.position.setValue({ x: gestureState.dx, y: gestureState.dy });
+      },
+      onPanResponderRelease: (e, gestureState) => {
+        if (gestureState.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe('right');
+        } else if (gestureState.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe('left');
+        } else {
+          this.resetPosition();
+        }
+      }
+    });
+
+    this.state = { index: 0 };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.data !== this.props.data) {
+      this.setState({
+        index: 0
+      })
+    }
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    this.position.setValue({ x: 0, y: 0 });
+    UIManager.setLayoutAnimationEnabledExperimental && 
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+    LayoutAnimation.spring();
+    this.setState({ index: this.state.index + 1 });
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(this.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  resetPosition() {
+    Animated.spring(this.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+  getCardStyle() {
+    const { position } = this;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-90deg', '0deg', '90deg']
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
+  // renderCardItem = (item, i) => {
+  //   if (!this.props.data.length) {
+  //     return this.props.renderNoMoreCards();
+  //   }
+  //   return ( //(i === 0) ? (
+  //     <Animated.View 
+  //       style={this.getCardStyle()}
+  //       key={item.petId}
+  //       {...this._panResponder.panHandlers}>
+  //       {this.props.renderCard(item)}
+  //     </Animated.View>
+  //   ) //: (
+  //     //<View key={item.petId}>{this.props.renderCard(item)}</View>
+  //   //);
+  // };
+
+  renderCards = () => {
+    if (this.state.index >= this.props.data.length) {
+      return this.props.renderNoMoreCards();
+    }
+
+    const deck = this.props.data.map((item, i) => {
+      if (i < this.state.index) { return null; }
+
+      if (i === this.state.index) {
+        return ( 
+          <Animated.View 
+            style={[this.getCardStyle(), styles.cardStyle, { zIndex: 99 }]}
+            key={item[this.props.keyProp]}
+            {...this._panResponder.panHandlers}>
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+
+
+      return (
+        <Animated.View key={item[this.props.keyProp]}
+          style={[styles.cardStyle, { top: 10 * (i - this.state.index), zIndex: 1 }]}>
+          {this.props.renderCard(item)}
+        </Animated.View>
+      );
+    })
+ 
+
+  return Platform.OS === 'android' ? deck.reverse() : deck.reverse();
+};
+
+  render() {
+    return <View>{this.renderCards()}</View>;
+  }
+};
+
+const styles = StyleSheet.create ({
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH,
+    
+  }
+});
+
+export default Swipe;
